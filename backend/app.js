1862533920c4c7397ba91e3213d9a0f3acc5ddec
Bhaskar Jha <bhaskar.kumar@utilitarianlab.com>
2021-07-08 21:57:33 +0530

    Order API

---
+++ backend/app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const path = require('path')
+const Razorpay = require('razorpay')
+const shortid = require('shortid')
+const cors = require("cors");
+
+const app = express()
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const razorpay = new Razorpay({
+	key_id: 'rzp_test_EOBUXhgUFzEzhu',
+	key_secret: 'pxU76GPMHWbRup5D9Nf40rN3'
+})
+
+
+const port = process.env.PORT || 8000;
+
+app.get('/logo.svg',(req,res)=>{
+    res.sendFile(path.join(__dirname,'logo.svg'))
+})
+
+app.post('/razorpay', async (req, res) => {
+	const payment_capture = 1
+	const amount = 499
+	const currency = 'INR'
+
+	const options = {
+		amount: amount * 100,
+		currency,
+		receipt: shortid.generate(),
+		payment_capture
+	}
+
+	try {
+		const response = await razorpay.orders.create(options)
+		console.log("Razorpay response "+JSON.stringify(response))
+		res.json({
+			id: response.id,
+			currency: response.currency,
+			amount: response.amount
+		})
+	} catch (error) {
+		console.log("Error From Razorpay "+JSON.stringify(error))
+	}
+})
+//Starting a server
+app.listen(port, () => {
+    console.log(`app is running at ${port}`);
+});
